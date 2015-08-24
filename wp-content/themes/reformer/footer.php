5da95bc2d489785a8ca30564f937e12be96d6c09
Nate Paul <nate.paul@cph.org>
2015-08-24 16:48:32 -0500

    Updated footer with signup and buttons

--- wp-content/themes/reformer/footer.php
+++ wp-content/themes/reformer/footer.php
@@ -20,8 +20,29 @@
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</div>
 				<div class="one-half column footer-navigation">
-					Other Footer stuff
+					<div class="footer-buttons">
+						<a href="#" class="button">Get Involved</a>
+						<a href="#" class="button">Shop</a>
+					</div>
+					<div class="footer-email-signup">
+						<label for="text" class="required">Text Input</label>
+						<input id="text" type="email" placeholder="Enter Email">
+	          <button type="submit" class="btn btn-primary">Sign Up</button>
+	        </div>
+	        <div class="footer-social-buttons">
+	        	Get Connected <a href="">Twitter</a><a href="">Facebook</a><a href="">Google+</a><a href="">Pinterest</a><a href="">Instagram</a><a href="">Vimeo</a>
+	        </div>
+	        <div class="footer-logos">
+	        	<span>LCMS Logo</span>
+	        	<span>CPH Logo</span>
+        	</div>
+        </div>
+			</div><!-- .row -->
+			<div class="row">
+				<div class="twelve columns footer-copyright">
+				© 2015 The Lutheran Church—Missouri Synod. All Rights Reserved. 	
 				</div>
+				
 			</div><!-- .row -->
 		</div><!-- .container -->
 	</footer><!-- #colophon -->
