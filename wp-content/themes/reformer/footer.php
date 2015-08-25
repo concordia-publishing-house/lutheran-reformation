ebba16d29105939631a4739967ddca2c275c7988
Nate Paul <nate.paul@cph.org>
2015-08-25 12:33:28 -0500

    updating footer css

--- wp-content/themes/reformer/footer.php
+++ wp-content/themes/reformer/footer.php
@@ -30,7 +30,7 @@
 	          <button type="submit" class="btn btn-primary">Sign Up</button>
 	        </div>
 	        <div class="footer-social-buttons">
-	        	Get Connected <a href=""><i class="icon-twitter"></i></a><a href=""><i class="icon-facebook"></i></a><a href=""><i class="icon-plus"></i></a><a href=""><i class="icon-pinterest"></i></a><a href=""><i class="icon-instagram"></i></a><a href=""><i class="icon-vimeo"></i></a>
+	        	Get Connected <a href=""><i class="icon-twitter"></i></a><a href=""><i class="icon-facebook"></i></a><a href=""><i class="icon-gplus"></i></a><a href=""><i class="icon-pinterest"></i></a><a href=""><i class="icon-instagram"></i></a><a href=""><i class="icon-vimeo"></i></a>
 	        </div>
 	        <div class="footer-logos">
 	        	<div class="lcms-logo-white"><a href="http://lcms.org" title="The Lutheran Church—Missouri Synod" target="_blank"></a></div>
