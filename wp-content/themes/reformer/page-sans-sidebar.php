6a043a4d2b1583ddba254062486c35104d534bc7
Nate Paul <nate.paul@cph.org>
2015-09-08 16:02:57 -0500

    removed .container for more control on fronted

--- wp-content/themes/reformer/page-sans-sidebar.php
+++ wp-content/themes/reformer/page-sans-sidebar.php
@@ -7,7 +7,6 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<div class="container">
 			<main id="main" class="site-main" role="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
@@ -24,7 +23,6 @@ get_header(); ?>
 				<?php endwhile; // End of the loop. ?>
 
 			</main><!-- #main -->
-		</div><!-- .container -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
