e43e2a729ac30c20d7ef46b4232dc22d7314351c
Nate Paul <nate.paul@cph.org>
2015-09-08 16:24:43 -0500

    updating page layout w/ sidebar

--- wp-content/themes/reformer/page-sans-sidebar.php
+++ wp-content/themes/reformer/page-sans-sidebar.php
@@ -6,7 +6,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="full-content-area">
 			<main id="main" class="site-main" role="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
