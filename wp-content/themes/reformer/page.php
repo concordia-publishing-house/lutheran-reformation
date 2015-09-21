79920407afe576ef55fd9db3f84bf775ea19740a
Nate Paul <nate.paul@cph.org>
2015-09-21 13:09:35 -0500

    fixing php error for featured image

--- wp-content/themes/reformer/page.php
+++ wp-content/themes/reformer/page.php
@@ -35,8 +35,7 @@ get_header(); ?>
 
 					<article id="<?php echo $page->post_name; ?>">
 						<div class="featured-image">
-							<?php echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );	}
-						?>
+							<?php echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' ); ?>
 						</div>
 						<div class="featured-content">
 							<h4><a href="<?php echo get_page_link($page->ID) ?>"><?php echo $page->post_title ?></a></h4>
