57cd7a10d743f7d2d2b729fde7f623a5428698c0
Nate Paul <nate.paul@cph.org>
2015-09-22 19:04:39 -0500

    fix - list child pages on homepage

--- wp-content/themes/reformer/page-sans-sidebar.php
+++ wp-content/themes/reformer/page-sans-sidebar.php
@@ -20,14 +20,14 @@ get_header(); ?>
 							</div>
 						</div>
 					</div>							
-
-				<?php endif; ?>
-
-				<?php query_posts(array('showposts' => 20, 'post_parent' => 7, 'post_type' => 'page')); while (have_posts()) { the_post(); ?>
+					<?php query_posts(array('showposts' => 20, 'post_parent' => 7, 'post_type' => 'page')); while (have_posts()) { the_post(); ?>
 
 				<h2><?php the_title(); ?></h2>
 
 				<?php } ?>
+				<?php endif; ?>
+
+				
 				
 				<?php while ( have_posts() ) : the_post(); ?>
 
