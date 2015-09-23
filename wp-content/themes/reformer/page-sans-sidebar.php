afe64bce5076c272e79ee8459492e5c602721b0e
Nate Paul <nate.paul@cph.org>
2015-09-22 21:51:02 -0500

    moved timeline into visual composer

--- wp-content/themes/reformer/page-sans-sidebar.php
+++ wp-content/themes/reformer/page-sans-sidebar.php
@@ -9,42 +9,6 @@ get_header(); ?>
 	<div id="primary" class="full-content-area">
 			<main id="main" class="site-main" role="main">
 
-				<?php if(is_front_page()) : ?>
-					<div class="countdown">
-						<div class="container">
-							<h5>Reformation Day 2017 is in</h5>
-							<div id="clock"></div>
-							<div class="countdown-buttons">
-								<a class="button" href="/events"><i class="icon-calendar"></i> View Events</a>
-								<a class="button" href="#"><i class="icon-mail"></i> Get Updates</a>
-							</div>
-						</div>
-					</div>							
-
-				<?php endif; ?>
-				<div class="sub-pages">	
-				<?php
-					$pages = get_pages('child_of='.$post->ID.'&sort_column=post_date&sort_order=desc&parent='.$post->ID);
-					foreach($pages as $page) {
-				?>
-				<?php //print_r($pages); ?>
-					<article id="<?php echo $page->post_name; ?>">
-						<div class="featured-image">
-							<?php echo '<a href="' . get_permalink( $page->ID ) . '" title="' . esc_attr( $page->post_name ) . '">';
-							echo get_the_post_thumbnail( $page->ID, 'medium'); 
-							echo "</a>";
-							?>
-						</div>
-						<div class="featured-content">
-							<h4><a href="<?php echo get_page_link($page->ID) ?>"><?php echo $page->post_title ?></a></h4>
-							<?php echo apply_filters('the_content', $page->post_excerpt); ?>
-						</div>
-
-					</article>
-
-				<?php } ?>
-			</div>
-				
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'template-parts/content', 'page' ); ?>
