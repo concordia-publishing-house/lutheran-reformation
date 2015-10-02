e740cf2232d5937934a1ef2cd04f5d533f23ac0d
Nate Paul <nate.paul@cph.org>
2015-10-02 16:51:42 -0500

    Removing full post from blog page, replacing with excerpt

--- wp-content/themes/reformer/template-parts/content.php
+++ wp-content/themes/reformer/template-parts/content.php
@@ -22,14 +22,7 @@
 
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'the-reformer' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
-
+		
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'the-reformer' ),
