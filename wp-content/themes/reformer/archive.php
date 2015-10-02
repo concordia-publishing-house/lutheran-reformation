12e70d0e80a932bc14a3507d66ea3dffd7818589
Nate Paul <nate.paul@cph.org>
2015-10-02 16:18:13 -0500

    updating blog archive.php

--- wp-content/themes/reformer/archive.php
+++ wp-content/themes/reformer/archive.php
@@ -8,9 +8,8 @@
  */
 
 get_header(); ?>
-
+<div class="container">
 	<div id="primary" class="content-area">
-		<div class="container">
 			<main id="main" class="site-main" role="main">
 
 			<?php if ( have_posts() ) : ?>
@@ -25,15 +24,7 @@ get_header(); ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', get_post_format() );
-					?>
+					<?php the_excerpt(); ?>
 
 				<?php endwhile; ?>
 
@@ -46,8 +37,8 @@ get_header(); ?>
 			<?php endif; ?>
 
 			</main><!-- #main -->
-		</div><!-- container -->
 	</div><!-- #primary -->
+</div><!-- container -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
