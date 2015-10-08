2060783fb0f9ba300747fe49bf53ec7a2eab97bb
Nate Paul <nate.paul@cph.org>
2015-10-08 08:46:33 -0500

    fixing double h1s on single.php

--- wp-content/themes/reformer/template-parts/content-single.php
+++ wp-content/themes/reformer/template-parts/content-single.php
@@ -10,8 +10,8 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<header class="post-entry-header">
+		<?php the_title( '<h1 id="page_header" class="entry-title">', '</h1>' ); ?>
 
 		<div class="entry-meta">
 			<?php the_reformer_posted_on(); ?>
