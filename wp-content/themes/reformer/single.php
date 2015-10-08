19566f05638852ce09f9ee9574c6de2dc4d7e6c0
Nate Paul <nate.paul@cph.org>
2015-10-08 08:43:35 -0500

    centering blog posts

--- wp-content/themes/reformer/single.php
+++ wp-content/themes/reformer/single.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 <div class="container">
-	<div id="primary" class="content-area">
+	<div id="primary" class="blog-post-area">
 		
 			<main id="main" class="site-main" role="main">
 
