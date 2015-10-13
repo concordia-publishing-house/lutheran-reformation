59fd05b499307fa10c00791bdf83e258f5cbd6b9
Nate Paul <nate.paul@cph.org>
2015-10-13 12:01:56 -0500

    customizing vimeo player

--- wp-content/themes/reformer/js/modal.js
+++ wp-content/themes/reformer/js/modal.js
@@ -20,7 +20,7 @@ jQuery(function() {
   jQuery("#modal").on("change", function() {
     if (jQuery(this).is(":checked")) {
       jQuery("body").addClass("modal-open");
-      jQuery("#remodal_video").html('<iframe src="http://player.vimeo.com/video/141601711" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
+      jQuery("#remodal_video").html('<iframe src="http://player.vimeo.com/video/141601711?portrait=0&badge=0&byline=0&title=0" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
     } else {
       jQuery("body").removeClass("modal-open");
       jQuery(".video-wrapper").html('&nbsp;');
