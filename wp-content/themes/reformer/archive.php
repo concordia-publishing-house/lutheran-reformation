f5f59918e413735c8568ff11c1d7fa64e6111524
Nate Paul <nate.paul@cph.org>
2016-11-02 12:09:09 -0500

    fixing archive.php

--- wp-content/themes/reformer/archive.php
+++ wp-content/themes/reformer/archive.php
@@ -24,7 +24,15 @@ get_header(); ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php the_excerpt(); ?>
+					<?php
+
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
+					?>
 
 				<?php endwhile; ?>
 
