b137c327cc1a902f498c2d3a1f63502429da8265
Nate Paul <nate.paul@cph.org>
2016-11-07 13:14:31 -0600

    adding link support for featured images

--- wp-content/themes/reformer/functions.php
+++ wp-content/themes/reformer/functions.php
@@ -44,6 +44,14 @@ function the_reformer_setup() {
 
 	add_image_size( 'featured_post_image_thumbnail', 800, 400, true );
 	
+	add_filter( 'post_thumbnail_html', 'my_post_image_html', 10, 3 );
+
+	function my_post_image_html( $html, $post_id, $post_image_id ) {
+
+  $html = '<a href="' . get_permalink( $post_id ) . '" title="' . esc_attr( get_post_field( 'post_title', $post_id ) ) . '">' . $html . '</a>';
+  return $html;
+
+	}
 
 
 	// This theme uses wp_nav_menu() in one location.
