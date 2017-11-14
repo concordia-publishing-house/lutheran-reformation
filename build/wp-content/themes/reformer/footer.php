90203882dbfd8fa43ccc9ba1ebd975154fcf0cec
Nate Paul <nate.paul@cph.org>
2017-11-14 15:08:53 -0600

    Adding Responsive Tables to layout

--- build/wp-content/themes/reformer/footer.php
+++ build/wp-content/themes/reformer/footer.php
@@ -84,5 +84,26 @@
   		jQuery("#remodal_video").html('<iframe src="https://player.vimeo.com/video/141601711" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
 		});
 	</script>
+	<script>
+			var headertext = [];
+			var headers = document.querySelectorAll("thead");
+			var tablebody = document.querySelectorAll("tbody");
+
+			for (var i = 0; i < headers.length; i++) {
+				headertext[i]=[];
+				for (var j = 0, headrow; headrow = headers[i].rows[0].cells[j]; j++) {
+				  var current = headrow;
+				  headertext[i].push(current.textContent);
+				  }
+			}
+
+			for (var h = 0, tbody; tbody = tablebody[h]; h++) {
+				for (var i = 0, row; row = tbody.rows[i]; i++) {
+				  for (var j = 0, col; col = row.cells[j]; j++) {
+				    col.setAttribute("data-th", headertext[h][j]);
+				  } 
+				}
+		}
+</script>
 </body>
 </html>
